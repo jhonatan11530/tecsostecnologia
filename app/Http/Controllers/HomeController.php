0f01eb296509954ad4e97a4ae81b7091b63d01f4
Jhonatan fernandez <jhonatan1153@hotmail.com>
2024-11-13 19:35:00 -0500

    se sube organizacion desc de las fechas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lots;
-use Illuminate\Http\Request;
-use Carbon\Carbon;
 class HomeController extends Controller
 {
     function dashboard()
     {
         $lots = Lots::join('customers','lots.customerCode','customers.code')
         ->select('lots.Lot_No','customers.name','lots.AWB','lots.Carrier','lots.Supplier','lots.country','lots.Inspection_Date','lots.url')
-        ->where('lots.Inspection_Date', '>=', Carbon::now()->subMonth())
+        ->orderBy('lots.created_at', 'desc')
         ->get();
         return view('dashboard.index',compact('lots'));
     }
